da6851a1b2eca99e8749377cfa841cb0d85d267a
Simple212 <simplescalper@tutanota.com>
2023-02-09 15:43:07 +0530

    2asdfasdf232

--- src/Components/main.js
+++ src/Components/main.js
@@ -298,10 +298,6 @@ const Main1= ({reverse,value2,change1,value3,value4,value5,value6,value7,value10
                 <Col className='col-md-5 text-black'>
                 <Button className={'btn1 b shadow-3'} style={{'background-color':`${value100912}`, 'color':`${value10059}`, 'border':`${value10057}`} } >Переменная ставка</Button>
                 </Col>
-                
-                <Col className='col-md-4 text-black'>
-                <Button className={'btn1 b shadow-3'} style={{'background-color':`${value100912}`, 'color':`${value10059}`, 'border':`${value10057}`} } >Фиксированная ставка</Button>
-                </Col>
             </Row>
 
             <Row className='mt-3'>
