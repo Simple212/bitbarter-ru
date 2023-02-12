1ce9e2fb81eb985f5532ef0014a7b375aa66a2c3
Simple212 <simplescalper@tutanota.com>
2023-02-12 21:06:48 +0530

    2asdfasasdfasdf232344df232

--- src/Components/main.js
+++ src/Components/main.js
@@ -265,6 +265,14 @@ const Main1= ({reverse,value2,change1,value3,value4,value5,value6,value7,value10
                             </Nav.Link>
                             
                             <Nav.Link href="#home" className='d-flex justify-content-center align-items-center navlink'>
+                            <Button className='br1 btn_login b shadow-4'>Войти</Button>
+                            </Nav.Link>
+                            
+                            <Nav.Link href="#home" className='d-flex justify-content-center align-items-center navlink'>
+                            <Button className='br1 btn_signup b shadow-4'>регистрация</Button>
+                            </Nav.Link>
+                            
+                            <Nav.Link href="#home" className='d-flex justify-content-center align-items-center navlink'>
                             
                             <Dropdown>
                             
